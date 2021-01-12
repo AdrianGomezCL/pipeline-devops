be64d72faa5e8732b102ff5c9d10e608f1fda6ce
Adrian <adriangomez.cl@gmail.com>
2021-01-12 14:16:14 -0300

    Mover Funcion a src

---
+++ src/Util.groovy
@@ -0,0 +1,12 @@
+package org.cl
+
+def validateStage(stage) {
+
+    def stages = params.stage.tokenize(';')
+    
+    if(stages.contains(stage) || stages.size()==0) return true
+    
+    return false
+}
+
+return this
